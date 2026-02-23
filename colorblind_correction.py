748a4a2bb110a77a8681bf8fea476c42afc1185f
jairajcj <jairajcj@users.noreply.github.com>
2026-02-23 09:10:00 +0530

    feat: add inverse LMS to RGB matrix

--- colorblind_correction.py
+++ colorblind_correction.py
@@ -7,3 +7,5 @@ class ColorBlindnessCorrector:
 # RGB to LMS matrix
 RGB_TO_LMS = np.array([[0.31399022, 0.63951294, 0.04649755],[0.15537241, 0.75789446, 0.08670142],[0.01775239, 0.10944209, 0.87256922]])
 
+LMS_TO_RGB = np.linalg.inv(RGB_TO_LMS)
+

677569d4eed60c24ca45de93276dde32a8782952
jairajcj <jairajcj@users.noreply.github.com>
2026-02-23 09:00:00 +0530

    feat: add Hunt-Pointer-Estevez LMS transformation matrix

--- colorblind_correction.py
+++ colorblind_correction.py
@@ -4,3 +4,6 @@ import numpy as np
 class ColorBlindnessCorrector:
     pass
 
+# RGB to LMS matrix
+RGB_TO_LMS = np.array([[0.31399022, 0.63951294, 0.04649755],[0.15537241, 0.75789446, 0.08670142],[0.01775239, 0.10944209, 0.87256922]])
+
